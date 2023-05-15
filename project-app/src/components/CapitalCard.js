0c7cdc26dcaed8dd3d5b54696e88a6b034537db2
Sarah Dean <sarah.schmidtdean@gmail.com>
2023-05-15 14:13:24 -0500

    component frmaework

---
+++ project-app/src/components/CapitalCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CapitalCard() {
+  return (
+    <div>CapitalCard</div>
+  )
+}
+
+export default CapitalCard
---
+++ project-app/src/components/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SearchBar from './SearchBar'
+
+function Menu() {
+  return (
+    <div>Menu
+      <SearchBar />
+    </div>
+  )
+}
+
+export default Menu
