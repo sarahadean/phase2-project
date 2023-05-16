0945754d3f548da26600a4d5ccf28e528f462778
Sarah Dean <sarah.schmidtdean@gmail.com>
2023-05-16 11:21:51 -0500

    card successfully rendered

--- project-app/src/components/CapitalCard.js
+++ project-app/src/components/CapitalCard.js
@@ -1,8 +1,46 @@
 import React from 'react'
 
-function CapitalCard() {
+// {
+//   "name": {
+//       "common": "Kuwait",
+//       "official": "State of Kuwait",
+//       "nativeName": {
+//           "ara": {
+//               "official": "دولة الكويت",
+//               "common": "الكويت"
+//           }
+//       }
+//   },
+
+//   "currencies": {
+//       "KWD": {
+//           "name": "Kuwaiti dinar",
+//           "symbol": "د.ك"
+//       }
+//   },
+
+//   "capital": [
+//       "Kuwait City"
+//   ],
+
+//   "region": "Asia",
+//   "flag": "🇰🇼"
+// },
+
+function CapitalCard({capitalInfo}) {
+  console.log(capitalInfo)
+
+  const {name: {common}, capital, region, flag} = capitalInfo
+
   return (
-    <div className='card'>CapitalCard</div>
+    <div className='card'>CapitalCard
+      <ul>
+      <li>Capital:{capital}</li>
+      <li>Country:{common}</li>
+      <li>Region:{region}</li>
+      <li>Flag:{flag}</li>
+      </ul>
+    </div>
   )
 }
 
