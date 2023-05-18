05454bed2b4cbecbc03e2e0899e7fb0baf699f2d
Zoe Guidry <oyezoey@gmail.com>
2023-05-18 14:24:10 -0500

    header-image compatible

--- project-app/src/components/Header.js
+++ project-app/src/components/Header.js
@@ -3,6 +3,7 @@ import React from "react";
 function Header () {
     return (
       <header className="App-header">
+      {/* <img src='./world-header.png' alt='world header image' /> */}
         <h1>PassportPal</h1>
       </header>
     )
